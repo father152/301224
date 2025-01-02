e1da8e67688dd47d73e9c43a1be1e723803b1a95
Your Name <tsimbalenko.vv@gmail.com>
2025-01-02 11:29:09 +0200

    56

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import "./footer.css"
 
 
 
  function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+    <h5>All Right Reserved 2024</h5>
+    </div>
   )
 }
 export {Footer}
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import "./main.css"
+import LoremIpsum from 'react-lorem-ipsum'
 
 
 
@@ -7,9 +8,23 @@ import React from 'react'
   function Main() {
   return (
     <div className='main'>
-    <div className='container-left'>1</div>
-    <div className='container-center'>2</div>
-    <div className='container-right'>3</div>
+    <div className='container-main'>
+    <div className='container-left'>
+    <h2>LEFT</h2>
+    <p><LoremIpsum /></p>
+    </div>
+    
+    <div className='container-center'>
+   <h2>CENTER</h2>
+   <p><LoremIpsum /><LoremIpsum /><LoremIpsum /></p>
+    </div>
+   
+    <div className='container-right'>
+    <h2>RIGHT</h2>
+    <p><LoremIpsum /></p>
+    
+    </div>
+    </div>
     </div>
   )
 }
