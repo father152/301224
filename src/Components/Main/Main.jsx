8dea2a473a1ce37f91244acdfa91470b182a9840
Your Name <tsimbalenko.vv@gmail.com>
2025-01-02 15:10:03 +0200

    12

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import "./main.css"
-import LoremIpsum from 'react-lorem-ipsum'
+import React from "react";
+import "./main.css";
+import LoremIpsum from "react-lorem-ipsum";
 
-
-
-
-  function Main() {
+function Main() {
   return (
-    <div className='main'>
-    <div className='container-main'>
-    <div className='container-left'>
-    <h2>LEFT</h2>
-    <p><LoremIpsum /></p>
+    <div className="main">
+      <div className="container-main">
+        <div className="container-left">
+          <h2>LEFT</h2>
+          <p>
+            <LoremIpsum />
+          </p>
+        </div>
+
+        <div className="container-center">
+          <h2>CENTER</h2>
+          <p>
+            <LoremIpsum />
+            <LoremIpsum />
+            <LoremIpsum />
+          </p>
+        </div>
+
+        <div className="container-right">
+          <h2>RIGHT</h2>
+          <p>
+            <LoremIpsum />
+          </p>
+        </div>
+      </div>
     </div>
-    
-    <div className='container-center'>
-   <h2>CENTER</h2>
-   <p><LoremIpsum /><LoremIpsum /><LoremIpsum /></p>
-    </div>
-   
-    <div className='container-right'>
-    <h2>RIGHT</h2>
-    <p><LoremIpsum /></p>
-    
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
-export {Main}
+export { Main };
