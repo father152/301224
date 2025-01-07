c223e76f1f413a1b806de3b0d83aabef34a269b4
Your Name <tsimbalenko.vv@gmail.com>
2025-01-07 09:51:05 +0200

    23

--- src/Components/Header/Catalogue/Catalogue.jsx
+++ src/Components/Header/Catalogue/Catalogue.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
-
+import women from "./woman-8597134_1280.png"
+import "../Catalogue/catalogue.css"
 
 function Catalogue() {
   return (
-    <div>Catalogue</div>
+    <div className='catalogue'>
+    <div className='container-catalogue'>
+    <div className='women'>
+    <img src= {women} alt=''></img>
+    </div>
+    </div>
+    
+    </div>
   )
 }
 export {Catalogue}
